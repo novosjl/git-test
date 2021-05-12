6b80f082ff061a5b608c71fd515422701df965b3
sujianlin <novo5g@outlook.com>
2021-05-12 18:56:53 +0800

    third commit

--- src/main/java/com/sjl/gitTest.java
+++ src/main/java/com/sjl/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("git-demo-idea");
         System.out.println("git---");
+        System.out.println("git___");
     }
 }
