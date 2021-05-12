33f7186a36eed15c6598f110f46b6e7efb72f56f
sujianlin <novo5g@outlook.com>
2021-05-12 19:04:45 +0800

    hot-master

--- src/main/java/com/sjl/gitTest.java
+++ src/main/java/com/sjl/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("git---");
         System.out.println("git___git--");
         System.out.println("hot-fit");
+        System.out.println("master");
     }
 }
